127d9f846a7e9d4bb3a3138a15dc6e70e2d5c81c
lbastoss <lucas.bastos.7@hotmail.com>
2025-09-23 15:13:35 -0300

    Tailwind Css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
+// biome-ignore lint/style/noNonNullAssertion: usage of non-null assertion is safe here
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
