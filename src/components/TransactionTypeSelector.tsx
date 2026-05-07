e7242119b42aabaca10e1c180e17e5dd245149a9
lbastoss <lucas.bastos.7@hotmail.com>
2026-05-07 14:54:28 -0300

    Adicionando Tipo de Transação

---
+++ src/components/TransactionTypeSelector.tsx
@@ -0,0 +1,49 @@
+import { TransactionType } from "../types/Transactions";
+
+interface TransactionTypeSelectorProps {
+  value: TransactionType;
+  id?: string;
+  onchange: (type: TransactionType) => void;
+}
+
+const TransactionTypeSelector = ({ value, id, onchange }: TransactionTypeSelectorProps) => {
+  const transactionsTypeButtons = [
+    {
+      type: TransactionType.EXPENSE,
+      label: "Despesa",
+      activeClasses: "bg-red-100 border-red-500 text-red-700 font-medium",
+      inactiveClasses: "br-transparent border-red-300 text-red-500 hover:bg-red-50",
+    },
+
+    {
+      type: TransactionType.INCOME,
+      label: "Receita",
+      activeClasses: "bg-green-100 border-green-500 text-green-700 font-medium",
+      inactiveClasses: "br-transparent border-green-300 text-green-500 hover:bg-green-50",
+    },
+  ];
+
+  return (
+    <fieldset id={id} className="grid grid-cols-2 gap-4">
+      <legend>Tipo de transação</legend>
+
+      {transactionsTypeButtons.map((item) => (
+        <button
+          key={item.type}
+          type="button"
+          onClick={() => onchange(item.type)}
+          className={`flex cursor-pointer items-center justify-center border rounded-md py-2 px-4 transition-all
+             ${value === item.type ? item.activeClasses : item.inactiveClasses}
+            
+            
+            
+            `}
+        >
+          {item.label}
+        </button>
+      ))}
+    </fieldset>
+  );
+};
+
+export default TransactionTypeSelector;
--- src/pages/TransactionsForm.tsx
+++ src/pages/TransactionsForm.tsx
@@ -1,7 +1,66 @@
+import { useEffect, useId, useState } from "react";
+import Card from "../components/Card";
+import TransactionTypeSelector from "../components/TransactionTypeSelector";
+import { getCategories } from "../services/categoryService";
+import type { Category } from "../types/category";
+import { TransactionType } from "../types/Transactions";
+
+interface FormData {
+  description: string;
+  amount: number;
+  type: TransactionType;
+  date: string;
+  category: string;
+}
+
+const initialFormData = {
+  description: "",
+  amount: 0,
+  type: TransactionType.EXPENSE,
+  date: "",
+  category: "",
+};
+
 const TransactionsForm = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+  const formId = useId();
+
+  useEffect(() => {
+    const fetchCategories = async (): Promise<void> => {
+      const response = await getCategories();
+      setCategories(response);
+    };
+    fetchCategories();
+  }, []);
+
+  const handleTransactionType = (itemType: TransactionType): void => {
+    setFormData((prev) => ({
+      ...prev,
+      type: itemType,
+    }));
+  };
+
+  const handleSubmit = () => {};
+
   return (
     <div>
-      <h1>Transaction Form</h1>
+      <div>
+        <h1>Transaction Form</h1>
+
+        <Card>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor={formId}>Tipo de despesa</label>
+              <TransactionTypeSelector
+                id={formId}
+                value={formData.type}
+                onchange={handleTransactionType}
+              />
+            </div>
+          </form>
+        </Card>
+      </div>
     </div>
   );
 };
